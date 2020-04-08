1f8dac3735ca6b367eb2daed00446e91a55be01f
Dmitry Sedyakin <42033012+usero4eg@users.noreply.github.com>
2020-04-08 14:34:38 +0300

    push mysql:db_prepared image to dirtygambrinus repo

--- mysql.sh
+++ mysql.sh
@@ -1,1 +1,12 @@
 docker exec -i -t mysql-server mysql -uroot -proot
+create database zabbix_db character set utf8 collate utf8_bin;
+create database grafana_db;
+create database nextcloud_db;
+# mysql> create user 'zabbix_us'@'localhost' identified by 'zabbix_pw;
+#        grant all privileges on zabbix_db.* to 'zabbix_us'@'localhost' with grant option;
+# mysql> create user 'grafana_us'@'localhost' identified by 'grafana_pw;
+#        grant all privileges on grafana_db.* to 'zabbix_us'@'localhost' with grant option;
+# mysql> create user 'nextcloud_us'@'localhost' identified by 'nextcloud_pw;
+#        grant all privileges on nextcloud_db.* to 'nextcloud_us'@'localhost' with grant option;
+docker tag <containter_id/image_id> dirtygambrinus/<container_id/image_id>
+docker push dirtygambrinus/<container_id/image_id>
